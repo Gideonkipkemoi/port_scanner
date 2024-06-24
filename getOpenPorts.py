7448a5149c2c7c3a865c3b29351784b799ebe119
GideonKipkemoi <rotichgideon760@gmail.com>
2024-06-24 21:34:50 +0300

    introduced pyfiglet for banner and date time

--- getOpenPorts.py
+++ getOpenPorts.py
@@ -1,13 +1,36 @@
 import socket
+import pyfiglet
+from datetime import datetime
+import sys
 
-target_ip = "127.0.0.1"
 
-for i in range(1000):
-    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    socket.setdefaulttimeout(0.001)
-    result = client.connect_ex((target_ip, i))
-    if result == 0:
-        print(f'Port {i} is open')
-    #else:
-        #print('Port is closed')
-client.close()
+ascii_banner = pyfiglet.figlet_format("PORT SCANNER")
+print(ascii_banner)
+
+target = input("Enter an ip: ")
+
+print("_" * 50)
+print(f"Scanning Target: {target}")
+print("Scanning started at: "+str(datetime.now()))
+print("_" * 50)
+
+try:
+    for port in range(1000):
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        socket.setdefaulttimeout(0.001)
+        result = client.connect_ex((target, port))
+        if result == 0:
+            print(f'Port {port} is open')
+        #else:
+            #print('Port is closed')
+    client.close()
+
+except KeyboardInterrupt:
+        print("\n Exiting Program !!!!")
+        sys.exit()
+except socket.gaierror:
+        print("\n Hostname Could Not Be Resolved !!!!")
+        sys.exit()
+except socket.error:
+        print("\ Server not responding !!!!")
+        sys.exit()
